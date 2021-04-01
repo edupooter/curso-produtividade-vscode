3f2a5a9fb5ad9c0ff5f4ccba375e54aa27a5f0ac
Eduardo Pooter Reis <edupooterreis@gmail.com>
2021-04-01 00:04:38 -0300

    Adiciona exemplo para treinar uso da extensão "TODO Tree".

--- index.js
+++ index.js
@@ -3,14 +3,17 @@ function helloWorld () {
   console.log('Hello World')
 }
 
+// TODO verificar horário noturno
 const saudacao = () => (new Date()).getHours() <= 12 ? 'Bom dia' : 'Boa tarde'
 
 helloWorld()
 
 console.log(saudacao())
 
+// FIXME remover variável sem uso
 const exemploDeExtensao = 'bem sucedida'
 
+// Manter variável
 const exemploDeVariavelSemUso = 'mal sucedida'
 
 const templateStringConverter = `Teste de conversão de ${exemploDeExtensao}`
